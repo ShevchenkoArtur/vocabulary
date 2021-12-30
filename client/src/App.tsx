04768766167f98791496b86efb5ce728acef10e6
Artur <shevchenkoartur745@gmail.com>
2021-12-30 19:41:00 +0200

    added container

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import VocabularyTable from './components/VocabularyTable/VocabularyTable';
+import { Container } from '@mui/material';
 
 function App() {
   return (
-    <>
+    <Container>
       <VocabularyTable />
-    </>
+    </Container>
   );
 }
 
